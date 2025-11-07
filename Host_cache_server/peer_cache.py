c5bc863a3f9c85c77631d0276377cb9b2cfb5415
cuongchuck <cuong.buiducmanh@gmail.com>
2025-11-07 23:55:11 +0700

    Update add peer, get subnetwork. Add function to generate multicast group

--- Host_cache_server/peer_cache.py
+++ Host_cache_server/peer_cache.py
@@ -1,12 +1,90 @@
-ultra_peers = []
+import math
+import config
 
-def add_ultra_peer(ip, port):
-    ultra_peers.append({"ip": ip, "port": port})
+# {ultra_id: [id], id: []}
+subnetworks = {}
 
-def get_ultra_peers():
-    return ultra_peers
+# store all nodes
+# {id: {ip, port, location, ultra}}
+peers = {}
 
+# node id
+peer_id = 0
+
+# multicast groups
+# [(id, [ip_octet1, ip_octet2, ip_octet3, ip_octet4], port)]
+groups = []
+
+def add_peer(ip, request):
+    global peer_id
+    port = request.get("port")
+    location = request.get("location")
+    ultra = request.get('rating') > config.RATING_THRESHOLD
+    if ultra:
+        subnetworks[peer_id] = []
+        groups.append(generate_multicast_group(peer_id))
+    else:
+        ultra_peer_found = False
+        if len(subnetworks) > 0:
+            min_dist = 0
+            ultra_peer = None
+            for key, value in peers.items():
+                if value['ultra'] and len(subnetworks[key]) < config.MAX_CHILDREN:
+                    dist = math.dist(location, value['location'])
+                    if dist < min_dist:
+                        min_dist = dist
+                        ultra_peer = key
+
+            if ultra_peer is not None:
+                subnetworks[ultra_peer] = peer_id
+                ultra_peer_found = True
+
+        if not ultra_peer_found:
+            subnetworks[peer_id] = []
+            groups.append(generate_multicast_group(peer_id))
+
+    peers[peer_id] = {"ip": ip, "port": port, "location": location, 'ultra': ultra}
+    peer_id += 1
+    return peer_id - 1
+
+# return ip, port of multicast group
+def get_subnetwork(id):
+    if id in subnetworks:
+        for _id, ip, port in groups:
+            if _id == id:
+                return ip, port
+    else:
+        for ultra_node, children in subnetworks.items():
+            if id in children:
+                for _id, ip, port in groups:
+                    if _id == ultra_node:
+                        return ip, port
+    return None, None
+
+def generate_multicast_group(id):
+    group = None
+    if len(groups) < 1:
+        group = (id, [224, 1, 1, 1], 50000)
+    else:
+        ip, port = groups[-1]
+        for i in range(-1, -4, -1):
+            if i == -4:
+                ip[i] = 224 if ip[i] == 239 else ip[i] + 1
+                break
+            if ip[i] < 255:
+                ip[i] += 1
+                break
+            else:
+                ip[i] = 1
+
+        group = (id, ip, port + 1)
+
+    groups.append(group)
+    return group
+
+
+# (function not done)
 def remove_ultra_peer(ip, port):
     peer = {"ip": ip, "port": port}
-    if peer in ultra_peers:
-        ultra_peers.remove(peer)
+    if peer in peers:
+        peers[peer] = None
