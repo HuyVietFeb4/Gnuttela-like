cee1de8c6620ad48aaa20cd6ca0dca7327d84e8b
cuongchuck <cuong.buiducmanh@gmail.com>
2025-12-09 14:01:24 +0700

    Use subprocess instead of socket, change max children to max nodes

--- Host_cache_server/config.py
+++ Host_cache_server/config.py
@@ -1,9 +1,9 @@
-import socket
-SERVER = socket.gethostbyname(socket.gethostname())
+import subprocess
+SERVER = subprocess.run("hostname -I", shell=True, capture_output=True, text=True).stdout.split(" ")[0]
 PORT = 5050
 ADDR = (SERVER, PORT)
 BOOTSTRAP_STATUS = ["accept", "reject"]
 MAX_PEERS = 100
 RATING_THRESHOLD = 2
-MAX_CHILDREN = 2
+MAX_NODES = 3
 MULTICAST_SIZE = 2
