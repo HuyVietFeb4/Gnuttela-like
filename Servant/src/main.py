20b03e1c0a4fe6d493a9a0adf5c508e885af7464
cuongchuck <cuong.buiducmanh@gmail.com>
2025-12-09 14:03:50 +0700

    Use grpc for bootstrap, add synchronize routing table

--- Servant/src/main.py
+++ Servant/src/main.py
@@ -1,38 +1,98 @@
-import sys
-sys.path.insert(0, '../config')
+import grpc, subprocess, threading
+from concurrent import futures
 
-import transport.bootstrap as bootstrap
-import application.peer_classification as classify
-import Host_cache_server.config as bootstrap_config
-from application.leaf_peer import Peer
-from application.ultra_peer import UltraPeer
-from application.bloom import BloomFilter
-import config.peer_settings as peer_settings
+from Servant.src.application.leaf_peer import Peer
+from Servant.src.application.ultra_peer import UltraPeer
+from Servant.src.application.bloom import BloomFilter
+from Servant.src.application.config import peer_settings
+from Servant.src.transport.network_table import network_table
+from Host_cache_server import config
+
+from Host_cache_server.grpc_bootstrap import bootstrap_pb2
+from Host_cache_server.grpc_bootstrap import bootstrap_pb2_grpc
+from Servant.src.transport.grpc_peer import peer_pb2
+from Servant.src.transport.grpc_peer import peer_pb2_grpc
+
+# Bootstrap client
+def request_bootstrap(ip):
+    with grpc.insecure_channel(f"{"192.168.1.6"}:{config.PORT + 1}") as channel:
+        stub = bootstrap_pb2_grpc.BootstrapStub(channel)
+        try:
+            response = stub.RequestBootstrap(bootstrap_pb2.JoinRequest(ip=ip, port=peer_settings.PORT))
+            print(f"Bootstrap response:\nSubnet ID: {response.subnetId}")
+            for node in response.subnet:
+                print(f"{node.ip}:{node.port}")
+            return response
+        except grpc.RpcError as e:
+            print("Request bootstrap failed:", e.code(), e.details())
+
+# PeerService gRPC server port = peer port + 1
+class PeerServiceServicer(peer_pb2_grpc.PeerServiceServicer):
+    def __init__(self, peer):
+        super().__init__()
+        self.peer = peer
+
+    def Announce(self, request, context):
+        print(f"Received announcement from leaf peer ({request.ip}:{request.port})")
+        self.peer.network_table.add_peer(request.ip, request.port, True)
+        ultra_ip, ultra_port = self.peer.network_table.get_ultra_peer()
+        return peer_pb2.UltraPeerAddress(ip=ultra_ip, port=ultra_port)
+
+    def ElectLeader(self, request, context):
+        return super().ElectLeader(request, context)
+
+def PeerServe(server_ip, server_port, peer):
+    servicer = PeerServiceServicer(peer)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    peer_pb2_grpc.add_PeerServiceServicer_to_server(servicer, server)
+    server.add_insecure_port(f"{server_ip}:{server_port + 1}")
+    server.start()
+    print(f"gRPC Peer server running on {server_port + 1}")
+    try:
+        server.wait_for_termination()
+    except KeyboardInterrupt:
+        print("Shutting down...")
+
+# Peer service client
+def announce_new_peer(server_ip, server_port, peer_ip, peer_port):
+    with grpc.insecure_channel(f"{server_ip}:{server_port + 1}") as channel:
+        stub = peer_pb2_grpc.PeerServiceStub(channel)
+        try:
+            response = stub.Announce(peer_pb2.NewPeerAddress(ip=peer_ip, port=peer_port))
+            print(f"Peer ({server_ip}:{server_port}) response:\nUltra peer address: ({response.ip}:{response.port})")
+            return response.ip, response.port
+        except grpc.RpcError as e:
+            print("Synchronization failed:", e.code(), e.details())
 
 if "__main__" == __name__:
-    peer = None
-    # True if leaf, False if ultra
-    peer_type = None
-    rating = classify.rating_based_on_bandwidth()
-    result = bootstrap.request_bootstrap(rating)
-    if rating > bootstrap_config.RATING_THRESHOLD:
-        peer_type = False
-        peer = UltraPeer(
-            BloomFilter(peer_settings.FILE_CAPACITY, peer_settings.ERROR_RATE),
-            peer_settings.HOST,
-            peer_settings.PORT,
-            result[0],
-            result[1],
-            result[2],
-            result[3]
-        )
-    else:
-        peer_type = True
-        peer = Peer(
-            BloomFilter(peer_settings.FILE_CAPACITY, peer_settings.ERROR_RATE),
-            peer_settings.HOST,
-            peer_settings.PORT,
-            result[0],
-            result[1]
-        )
+    # Get ip address
+    result = subprocess.run("hostname -I", shell=True, capture_output=True, text=True)
+    ip = result.stdout.split(" ")[0]
 
+    # Bootstrap
+    result = request_bootstrap(ip)
+
+    # Initialize peer
+    peer = Peer()
+    if len(result.subnet) == 1:
+        peer = UltraPeer()
+    peer.subnet_id = result.subnetId
+    peer.network_table = network_table()
+    if peer.__class__ == UltraPeer:
+        peer.network_table.add_peer(result.subnet[0].ip, result.subnet[0].port, False)
+    else:
+        [peer.network_table.add_peer(node.ip, node.port, True) for node in result.subnet]
+    
+    # Announce new peer with other peers
+    if peer.__class__ == Peer:
+        for node in peer.network_table.get_peers_addresses():
+            ultra_ip, ultra_port = announce_new_peer(node[0], node[1], ip, peer_settings.PORT)
+            peer.network_table.update_peer_role(ultra_ip, ultra_port, False)
+    
+    # Run PeerService server
+    PeerServiceThread = threading.Thread(target=PeerServe, args=(ip, peer_settings.PORT, peer))
+    PeerServiceThread.start()
+    try:
+        PeerServiceThread.join()
+    except KeyboardInterrupt:
+        pass
