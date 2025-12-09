fe2641bf9be77e3705b4a47d3c66b336ac01c3db
cuongchuck <cuong.buiducmanh@gmail.com>
2025-12-09 14:07:53 +0700

    Use ip address in config file

--- Servant/config/peer_settings.py
+++ Servant/config/peer_settings.py
@@ -1,4 +1,6 @@
+import subprocess
+
 ERROR_RATE = 0.01
 FILE_CAPACITY = 1000
-PORT = 50001
-HOST = "127.0.0.1"
+PORT = 8001
+HOST = subprocess.run("hostname -I", shell=True, capture_output=True, text=True).stdout.split(" ")[0]
