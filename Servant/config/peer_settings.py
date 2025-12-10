97d50efba3be3d833ff29bd1021bc75d80491b75
cuongchuck <cuong.buiducmanh@gmail.com>
2025-12-10 08:07:46 +0700

    Add file directory, retrieve file names

--- Servant/config/peer_settings.py
+++ Servant/config/peer_settings.py
@@ -1,6 +1,9 @@
-import subprocess
+import subprocess, os
+from pathlib import Path
 
 ERROR_RATE = 0.01
 FILE_CAPACITY = 1000
 PORT = 8001
 HOST = subprocess.run("hostname -I", shell=True, capture_output=True, text=True).stdout.split(" ")[0]
+FILE_DIRECTORY = Path(__file__).parent.parent / "files"
+FILES = [entry for entry in os.listdir(FILE_DIRECTORY) if os.path.isfile(os.path.join(FILE_DIRECTORY, entry))]
