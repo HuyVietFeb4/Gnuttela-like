80af7650e4ef1aa9ab9aeddf708fd9032e5ee4bb
cuongchuck <cuong.buiducmanh@gmail.com>
2025-12-24 13:14:11 +0700

    Add bloom_table

--- Servant/src/application/ultra_peer.py
+++ Servant/src/application/ultra_peer.py
@@ -1,5 +1,6 @@
 from Servant.src.application.leaf_peer import Peer
 
 class UltraPeer(Peer):
-    def __init__(self, bloom_filter=None, network_table=None, subnet_id=None):
+    def __init__(self, bloom_filter=None, network_table=None, subnet_id=None, bloom_table=None):
         super().__init__(bloom_filter, network_table, subnet_id)
+        self.bloom_table = bloom_table
